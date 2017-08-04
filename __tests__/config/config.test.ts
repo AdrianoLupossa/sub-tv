ee95c4fd9ee9534d17be1f58be2004063655b1dc
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-04 15:49:07 +0100

    Indented file and fix path of imports

--- __tests__/config/config.test.ts
+++ __tests__/config/config.test.ts
@@ -1,16 +1,18 @@
 import path = require('path')
 import { URL } from 'url'
 
-import Configuration from '../../config/config'
+import Configuration from '../../ts/config/config'
 
 describe('Configuration FIle', () => {
   test('Should return version from Package Json', () => {
-    const packageJsonPath = path.join(__dirname, '../../../package.json')
+    const packageJsonPath = path.join(__dirname, '../../package.json')
     const versionFromPackage = require(packageJsonPath).version
 
     expect(Configuration.version).toEqual(versionFromPackage)
   })
   test('Should return the actual provider', () => {
-    expect(Configuration.providersList).toEqual([new URL('https://www.tv-subs.com')])
+    expect(Configuration.providersList).toEqual([
+      new URL('https://www.tv-subs.com'),
+    ])
   })
 })
